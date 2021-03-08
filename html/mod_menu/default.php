03b6678d8c424fd25f03c907da6233997fe9e8b5
gabriel0369 <gabrielgf@protonmail.com>
2021-03-08 18:45:22 -0300

    first commit

---
+++ html/mod_menu/default.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$doc       = JFactory::getDocument();
+$direction = $doc->direction == 'rtl' ? 'pull-right' : '';
+$class     = $enabled ? 'nav ' . $direction : 'nav disabled ' . $direction;
+
+// Recurse through children of root node if they exist
+$menuTree = $menu->getTree();
+$root     = $menuTree->reset();
+
+if ($root->hasChildren())
+{
+	echo '<ul id="menu" class="' . $class . '">' . "\n";
+
+	// WARNING: Do not use direct 'include' or 'require' as it is important to isolate the scope for each call
+	$menu->renderSubmenu(JModuleHelper::getLayoutPath('mod_menu', 'default_submenu'));
+
+	echo "</ul>\n";
+
+	echo '<ul id="nav-empty" class="dropdown-menu nav-empty hidden-phone"></ul>';
+
+	if ($css = $menuTree->getCss())
+	{
+		$doc->addStyleDeclaration(implode("\n", $css));
+	}
+}
+
+$user      = JFactory::getUser();
+$AllGroups = $user->get('groups');
+// getting all the groups
+
+foreach($AllGroups as $groupId) {
+$user   = JFactory::getUser(); // getting user data
+$db     = JFactory::getDBO();
+$userid = $user->get('id'); // logged in user id
+$groups = JAccess::getGroupsByUser($userid);
+$groupid_list      = '(' . implode(',', $groups) . ')';
+$query  = $db->getQuery(true);
+$query->select('id, title');
+$query->from('#__usergroups');
+$query->where('id IN ' .$groupid_list);
+$db->setQuery($query);
+$rows   = $db->loadRowList();
+$grouplist   = '';
+foreach($rows as $group)
+{
+if ($groupId == $group[0]) { // checking the logged in user group
+$grouplist = $group[1];
+}
+}
+if ($grouplist == "Super Users") {
+?>
+<script type="text/javascript">
+window.onload=function(){
+var menuFeatured = document.querySelector(".menu-featured");
+ menuFeatured.style.display = "inline-block";
+ menuFeatured.style.position = "static";
+ menuFeatured.style.pointerEvents = "all";
+}
+
+</script>
+<?php
+}
+}
